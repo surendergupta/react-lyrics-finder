d9dcd0c7c2b0df4daf4dec7b9f89274bc202b739
surendergupta <gupta.surender.1990@gmail.com>
2025-07-27 15:27:30 +0530

    Empty Lyric finder app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+const App = () => {
+
+  return (
+    <>
+      <div className='App'>
+        <h1>Recat Lyrics Finder App!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
